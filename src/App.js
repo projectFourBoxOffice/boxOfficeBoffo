d0df2d0ebb2fdcc88071d7142505f6e4647cdff5
victorialange <viclan2009@hotmail.com>
2022-12-19 13:37:07 -0500

    added in rel=noopener and rel=noreferrer (for older browser) to ensure security for older browsers (netlify won't allow deploy otherwise)

--- src/App.js
+++ src/App.js
@@ -840,7 +840,7 @@ function App() {
       }
 
       <footer className={`${allFilteredMovies.length === 0 || listSubmit ? "noMovies" : ""} `}>
-          <p>Created @ <a href="https://junocollege.com/" target="_blank">Juno College of Technology</a></p>
+          <p>Created @ <a href="https://junocollege.com/" target="_blank" rel="noopener noreferrer">Juno College of Technology</a></p>
       </footer>
       
 
